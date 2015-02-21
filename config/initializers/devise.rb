2af95272913fbe7b180f24e463d6447ec3a41283
Michael Johann <tobiasriegel@t-online.de>
2015-02-21 12:49:15 +0100

    Root page ist jetzt das Sign In Formular. Adminlogik noch nicht komplett.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -176,7 +176,7 @@ Devise.setup do |config|
   # config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  # config.unlock_in = 1.hour
+  config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
   # config.last_attempt_warning = true
@@ -205,11 +205,11 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
-  # config.default_scope = :user
+  config.default_scope = :user
 
   # Set this configuration to false if you want /users/sign_out to sign out
   # only the current scope. By default, Devise signs out all scopes.
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,20 @@ Rails.application.routes.draw do
   devise_for :users
   resources :topics
 
+  devise_scope :user do
+    authenticated :user do
+      root 'topics#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'topics#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
