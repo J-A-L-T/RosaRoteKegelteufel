ce285f962f04d427ba35cacdd29d6934d39765c0
HansWurst90 <Jan@mussenbrock-w.de>
2015-02-23 10:03:10 -0700

    Validations für Topic und Comment hinzugefügt

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :topic
   belongs_to :user
+  belongs_to :topic
+
+  validates_associated :user
+  validates :body, presence: true, length: { maximum: 500 }
 end
