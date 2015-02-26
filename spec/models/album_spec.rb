4ac4157b8379b048df499feb5a11c095ad0b8211
Michael Johann <tobiasriegel@t-online.de>
2015-02-27 00:40:40 +0100

    Rspec Tests fuer Album hinzugefuegt

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Album, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Album do
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:album)).to be_valid
+  end
+  it 'is invalid without a title' do
+    expect(FactoryGirl.build(:album, title: nil)).to_not be_valid
+  end
+  it 'is invalid without a content' do
+    expect(FactoryGirl.build(:album, description: nil)).to_not be_valid
+  end
+  it 'is invalid with to much characters for title' do
+    expect(FactoryGirl.build(:album, title: Faker::Lorem.characters(51))).to_not be_valid
+  end
+  it 'is invalid with to much characters for content' do
+    expect(FactoryGirl.build(:album, description: Faker::Lorem.characters(2001))).to_not be_valid
+  end
 end
