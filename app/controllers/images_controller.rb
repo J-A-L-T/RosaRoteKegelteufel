9938fcad618accaab75d819dd3130c2e8e12ff62
PiCookie <l.erfkaemper@gmx.de>
2015-02-27 00:05:10 +0100

    Album ist fast fertig, Versuche noch Bilder nebeneinander anstatt nur untereinander zu bekommen und das Albenvorschaubild zu erzeugen

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,18 @@
 class ImagesController < InheritedResources::Base
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+  def destroy
+  @image.destroy
+      respond_to do |format|
+      format.html { redirect_to :back, notice: 'Image was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
+  def set_image
+    @image = Image.find(params[:id])
+  end
 
   def image_params
     params.require(:image).permit(:title, :album_id, :file)
