65562843a695cd43b4ec0e51d40292f4fb826e54
Michael Johann <tobiasriegel@t-online.de>
2015-02-26 15:06:24 +0100

    Rspec Test fuer Topics erweitert

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -10,4 +10,10 @@ describe Topic do
   it 'is invalid without a content' do
     expect(FactoryGirl.build(:topic, content: nil)).to_not be_valid
   end
+  it 'is invalid with to much characters for title' do
+    expect(FactoryGirl.build(:topic, title: Faker::Lorem.characters(51))).to_not be_valid
+  end
+  it 'is invalid with to much characters for content' do
+    expect(FactoryGirl.build(:topic, content: Faker::Lorem.characters(2001))).to_not be_valid
+  end
 end
